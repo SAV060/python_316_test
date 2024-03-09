cc9ef9447983cabf8f340e523a494995e92e3f43
SAV060 <svlv76@gmail.com>
2024-03-10 00:48:20 +0300

    test12:Рубрика "Эксперименты"

--- main.py
+++ main.py
@@ -5,4 +5,15 @@ print("Что-то сломалось")
 print("Ураааа,починилось!!!!!")
 # Вроде ничего не поменялось
 print("Всем привет 456 раз")
+# Что-то поменялось
 
+# import json
+# #
+cities_set = {city["name"] for city in cities}
+print(sorted(cities_set))
+# # print(type(cities_set))
+#
+#
+# with open('cities_set.json', 'w', encoding='utf-8') as file:
+#     json.dump(cities_set, file, ensure_ascii=False, indent=4)
+# #
