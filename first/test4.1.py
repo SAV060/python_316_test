3dc8a575a704a850432a74fe57577e052e49d560
SAV060 <svlv76@gmail.com>
2024-03-10 23:44:10 +0300

    добавлен модуль 4.2,удален "text",добавлена функция "multplay"

--- first/test4.1.py
+++ first/test4.1.py
@@ -1,11 +1,13 @@
 def plus(a, b):
     return a + b
 
-text = "Hello, World!"
-
 def minus(a, b):
     return a - b
+
+def multiply(a, b):
+    return a * b
 
 if __name__ == '__main__':
     print(plus(56, 2))
     print(minus(56, 2))
+    print(multiply(56, 2))
